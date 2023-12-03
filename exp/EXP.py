a96904c84b9271bfeb5e5ab305c7753b881f417e
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2023-12-03 12:38:31 +0100

    Add files via upload

--- exp/EXP.py
+++ exp/EXP.py
@@ -34,11 +34,36 @@ class EXPSystem:
         fk = EXPSystem.sys_factorial(k)
         fnk = EXPSystem.sys_factorial(n-k)
         return fn/(fk*fnk)
+    
+    def sys_binomial_distribution(n:int, p:float, k:int):
+        """P(X=K) = (nCk)*p^k(1-p)^(n-k)"""
+        nCk = EXPSystem.sys_binomial_coefficient(n, k)
+        PXK = nCk * p**k * (1-p)**(n-k)
+        return PXK
 
 "PUBLIC (CALCULATORS)"
 
-def factorial(n:int):
+def factorial():
+    n = int(input("factorial n : "))
     print(EXPSystem.sys_factorial(n))
-def binomial_coefficient(n:int, k:int):
+
+def binomial_coefficient():
+    n = int(input("n : "))
+    k = int(input("k : "))
     print(EXPSystem.sys_binomial_coefficient(n, k))
 
+def binomial_distribution():
+    """
+    >=, <=, >, < not available yet.
+    """
+    sign = str(input("Sign (=, >=, <=, >, <): "))
+    if sign not in [">","<","<=",">=","="]:
+        print("Error - Sign does not correspond")
+    else:
+        n = int(input("n : "))
+        p = float(input("p : "))
+        k = int(input("k : "))
+        print("P(X="+str(k)+") "+ sign + ' ' + str(EXPSystem.sys_binomial_distribution(n,p,k)))
+
+binomial_distribution()
+    
